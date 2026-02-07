4c1f44fd2b1597d391fc214feb610e7f7700b6c2
Paras Sahu <parastop5468@gmail.com>
2026-02-07 11:41:11 +0530

    contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
     }
 
     try{
-        const url =  "http://localhost:8080/contact/insert";
+        const url =  "https://auth-login-signin.vercel.app/contact/insert";
                     const response = await fetch(url, {
                         method:"POST",
                         headers: {
